b61902de2914ee24472dca9b4612b614921d889a
Sathisks23 <sathishksdckap@gmail.com>
2025-02-11 16:27:57 +0530

    robots edited

--- src/app/robots.txt/route.js
+++ src/app/robots.txt/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const robots = `User-agent: *
 Allow: /
-Sitemap: https://sathishdiaries.vercel.app/sitemap`;
+Sitemap: https://sathishdiaries.vercel.app/sitemap.xml`;
 
   return new NextResponse(robots, {
     headers: {
