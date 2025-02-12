b8eef1f23db042c4552d3867eaeabf6d2671f18d
Sathisks23 <sathishksdckap@gmail.com>
2025-02-12 11:16:40 +0530

    tag added for seo

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sathishdiaries.vercel.app', // Your website URL
+    generateRobotsTxt: true, // Generates robots.txt automatically
+    sitemapSize: 5000,
+  };
+  
